566d854545a84edcffe2d8c0282a181f017a4047
Dini <dini@Dinis-MacBook-Air.local>
2023-12-07 03:37:26 +0800

    install vercel

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://productivity-workspace-frontend.vercel.app"]
+    origin: ["https://productivity-workspace.vercel.app"]
   })
 );
 
